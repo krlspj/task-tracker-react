fc3efd1c7730caad2fedfd239eaaa03964691afb
krlspj <cpujades@albirasolutions.com>
2021-11-20 16:04:10 +0100

    add new component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  //const n1 = prompt('type ur name')
+  const name = 'Cyatile'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1>Hello from react</h1>
+      <h2>heheh... {name} </h2>
+      <Header title='leti' />
+
+{/*    <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+  */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.title}</h1>			
+		</div>
+	)
+}
+
+Header.defaultProps = {
+	title: 'Task Tracker',
+}
+
+export default Header

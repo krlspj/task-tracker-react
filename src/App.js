ea13ba66d1f474c0748d242d88d927a3c56b8c4f
krlspj <krls.pjds@gmail.com>
2021-11-23 00:54:23 +0100

    add crud methods to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask'
 import Footer from './components/Footer'
@@ -7,31 +7,86 @@ import About from './components/About'
 import { BrowserRouter as BRouter, Routes,
          Route } from 'react-router-dom'
 
+// Gloval Const
+const baseUrl = 'http://localhost:5000'
+
 
 function App() {
   //const n1 = prompt('type ur name')
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks,setTasks] = useState([
-    
-  ])
+  const [tasks,setTasks] = useState([])
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  },[])
+
+  // Fetch tasks from server
+  const fetchTasks = async () => {
+    const res = await fetch(baseUrl + '/tasks')
+    const data = await res.json()
+
+    //console.log('data',data)
+    return data
+  }
+
+  // Fetch task from server
+  const fetchTask = async (id) => {
+    const res = await fetch(baseUrl + `/tasks/${id}`)
+    const data = await res.json()
+
+    //console.log('data',data)
+    return data
+  }
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(baseUrl + `/tasks/${id}`,{
+      method: 'DELETE'
+    })
     //console.log('delete',id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
   
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle,reminder:!taskToToggle.reminder}
+    const res = await fetch(baseUrl + `/tasks/${id}`,{
+      method: 'PUT',
+       headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
     //console.log('id:',id)
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    //setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
   }
 
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(new Date().getTime())
-    const newTask =  {id, ...task}
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch(baseUrl + '/tasks', {
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    
+    const data = await res.json()
+
+    // add the new task to the existing tasks on the app
+    // so there is no need to fetch tasks again from the server
+    setTasks([...tasks, data])
+    //const id = Math.floor(new Date().getTime())
+    //const newTask =  {id, ...task}
+    //setTasks([...tasks, newTask])
   }
 
   // Contains the logic of showing the task components
